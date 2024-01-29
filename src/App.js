a85a2a60c6c6eaf7f6a36e765191d0a9e13d5284
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-29 21:15:25 +0530

    add select element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 1, description: " Passports", quantity: 2, packed: false },
+  { id: 2, description: " Socks", quantity: 12, packed: false },
+  { id: 3, description: " Charger", quantity: 1, packed: true },
 ];
 
 function App() {
@@ -20,9 +21,18 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip ? 😄</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +41,7 @@ function Packinglist() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -39,7 +49,15 @@ function Packinglist() {
 }
 
 function Item({ item }) {
-  return <li>{item.description}</li>;
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
