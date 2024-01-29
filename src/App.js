6f3d26480c9b03a0d14497b5be57d25b38bb68e7
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-29 20:13:50 +0530

    add Item() component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -22,8 +27,21 @@ function Form() {
 }
 
 function Packinglist() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
 function Stats() {
   return (
     <footer className="stats">
