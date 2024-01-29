956480d9bef0dac5a5af9954c5cd3a0668491032
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-29 19:45:36 +0530

    layout 🔥

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 function App() {
   return (
     <div className="App">
-
+      <Logo />
+      <Form />
+      <Packinglist />
+      <Stats />
     </div>
   );
+}
+
+function Logo() {
+  return <h1>✈️ Travel List🧳</h1>;
+}
+
+function Form() {
+  return <div></div>;
+}
+
+function Packinglist() {
+  return <div></div>;
+}
+function Stats() {
+  return <div></div>;
 }
 
 export default App;
